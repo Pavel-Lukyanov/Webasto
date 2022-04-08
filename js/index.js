99413963d76dfc6e3a6982fd9925808c3eaf8c38
Pavel-Lukyanov <97879761+Pavel-Lukyanov@users.noreply.github.com>
2022-04-08 11:55:05 +0500

    add css

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// Карта
+ymaps.ready(init);
+        function init(){
+            var myMap = new ymaps.Map("map", {
+                center: [61.262021, 73.458142],
+                zoom: 17,
+            });
+            var myPlacemark = new ymaps.Placemark([61.262021, 73.458142], {}, {
+                iconLayout: 'default#image',
+                iconImageSize: [35, 42],
+                iconImageOffset: [-3, -42],
+            });
+            myMap.geoObjects.add(myPlacemark);
+        }
